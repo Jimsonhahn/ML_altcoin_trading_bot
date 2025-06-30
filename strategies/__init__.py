fc00edc0136ac12b040cef26fee3ef0e908a7855
Jimsonhahn <164743378+Jimsonhahn@users.noreply.github.com>
2025-07-01 01:08:14 +0200

    Initial commit

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,29 +1,16 @@
-python3 << 'EOF'
-content = '''# strategies/__init__.py
+# strategies/__init__.py
 """Trading Strategies Module"""
 
-# Import strategies with correct filenames
 from .momentum import MomentumStrategy
 from .mean_reversion import MeanReversionStrategy
 from .ml_strategy import MLStrategy
 
-# Try to import advanced strategies
-try:
-    from .advanced import AdvancedStrategy
-    ADVANCED_AVAILABLE = True
-except ImportError:
-    ADVANCED_AVAILABLE = False
-    AdvancedStrategy = None
-
-# Strategy mapping
 STRATEGY_MAP = {
     "momentum": MomentumStrategy,
     "mean_reversion": MeanReversionStrategy,
     "ml": MLStrategy,
 }
 
-if ADVANCED_AVAILABLE:
-    STRATEGY_MAP["advanced"] = AdvancedStrategy
 
 def get_strategy(strategy_name, config):
     """Factory function to get strategy instance"""
@@ -33,11 +20,3 @@ def get_strategy(strategy_name, config):
 
     strategy_class = STRATEGY_MAP[strategy_name]
     return strategy_class(config)
-
-__all__ = ["get_strategy", "STRATEGY_MAP", "MomentumStrategy", "MeanReversionStrategy", "MLStrategy"]
-'''
-
-with open('strategies/__init__.py', 'w') as f:
-    f.write(content)
-print("strategies/__init__.py wurde erfolgreich erstellt!")
-EOF
